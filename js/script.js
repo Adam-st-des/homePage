de92cb2b67bb359fae8f458733880e2b459229e1
Adam Strzeciwilk <strzeciwilkadam@gmail.com>
2022-05-20 18:27:20 +0200

    start a color change

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
+const body= document.querySelector(".body__page");
+const button= document.querySelector(".body__button");
+const word= document.querySelector(".themeName");
+const change= document.querySelector(".changeColor");
 
  button.addEventListener("click", () => {
     body.classList.toggle("changeColor")
-   if(body.classList.contains("changeColor")){word.innerHTML="jasnoróżowy"}
-    else{word.innerHTML="zielony"}
+   if(body.classList.contains("changeColor")){word.innerHTML="różowy"}
+    else{word.innerHTML="jasnoróżowy"}
  })
