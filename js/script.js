5cd5be10842731cc9067938e11b0fde565e1102a
Adam Strzeciwilk <strzeciwilkadam@gmail.com>
2022-05-20 18:05:46 +0200

    js added

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+ button.addEventListener("click", () => {
+    body.classList.toggle("changeColor")
+   if(body.classList.contains("changeColor")){word.innerHTML="jasnoróżowy"}
+    else{word.innerHTML="zielony"}
+ })
